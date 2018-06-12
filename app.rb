0029a8ab4a5c4e92d0e74b01ca20d49e9f67c4d3
Egutierrez055 <egutierrez055@gmail.com>
2018-06-12 17:50:14 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+get '/name' do
+  "My name is  Elizabeth Gutierrez-Gutierrez"
 end
+get '/hometown' do
+  "My hometown is Denver, CO"
+end
+get '/favorite-song' do
+  "My favorite song is Mientes by Camila"
+end
+
+end 
+
